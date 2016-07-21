a2ac1954ded95a7ea392402295f3d3b2c6155434
aggietallboy <aggietallboy@hotmail.com>
2016-07-21 12:14:45 -0500

    Update HunterBeastMastery.lua
    Automatically switch talents (why, better display of what's happening)

--- GS-AggietallboyMacros/Hunter/BeastMastery-253/HunterBeastMastery.lua
+++ GS-AggietallboyMacros/Hunter/BeastMastery-253/HunterBeastMastery.lua
@@ -3,30 +3,66 @@ local Sequences = GSMasterSequences
 ------------------
 ----- Hunter
 ------------------
-Sequences['PUT_NAME_HERE'] = {
+Sequences['ATB_HUNT_BMSing'] = {
 specID = 253,
-author = "WHO_WROTE_IT",
-helpTxt = "INFO ABOUT MACRO/Title - Talent: 3212233",
+author = "Aggietallboy",
+helpTxt = "Single Target - Talent: 2322111",
 StepFunction = GSStaticPriority,
 PreMacro = [[
+/console Sound_EnableSFX 0
+/script UIErrorsFrame:Hide()
 /targetenemy [noharm][dead]
-/startattack
-/petattack [@target,harm]
-/petautocastoff [group] Growl
-/petautocaston [nogroup] Growl
+/cast [nochanneling,nopet,nodead]Call Pet 1;[nochanneling,@pet,dead]Heart of the Phoenix
+/petautocastoff [nochanneling,group] Growl
+/petautocaston [nochanneling,nogroup] Growl
+/cast [target=focus, exists, nodead],[target=pet, exists, nodead] Misdirection
+
+]],
+"/cast [nochanneling] Bestial Wrath",
+"/cast [nochanneling,talent:2/3] !Chimaera Shot",
+"/cast [nochanneling,talent:2/1] Dire Beast; [nochanneling,talent:2/3] Dire Beast; [nochanneling,talent:2/2] !Dire Frenzy",
+"/cast [nochanneling,talent:6/1] A Murder of Crows; [nochanneling,talent:6/2] Barrage; ",
+"/cast [nochanneling] Titan's Thunder",
+"/cast [nochanneling] Cobra Shot",
+PostMacro = [[
+/cast [combat,nochanneling]Aspect of the Wild
+/cast [combat,nochanneling] Blood Fury
+/use [combat]13
+/use [combat]14
+/script UIErrorsFrame:Clear()
+/console Sound_EnableSFX 1
+]],
+}
+
+
+
+Sequences['ATB_HUNT_BMAoE'] = {
+specID = 253,
+author = "Aggietallboy",
+helpTxt = "With Barrage - Talent: 3222321",
+StepFunction = GSStaticPriority,
+PreMacro = [[
+/console Sound_EnableSFX 0
+/script UIErrorsFrame:Hide()
+/targetenemy [noharm][dead]
+/cast [nochanneling,nopet,nodead]Call Pet 1;[nochanneling,@pet,dead]Heart of the Phoenix
+/petautocastoff [nochanneling,group] Growl
+/petautocaston [nochanneling,nogroup] Growl
 /cast [target=focus, exists, nodead],[target=pet, exists, nodead] Misdirection
 ]],
 "/cast [nochanneling] Bestial Wrath",
-"/cast [nochanneling] !Kill Command",
-"/cast [nochanneling] Dire Frenzy",
 "/cast [nochanneling] Multi-Shot",
+"/cast [nochanneling,talent:2/3] !Chimaera Shot",
+"/cast [nochanneling,talent:2/1] Dire Beast; [nochanneling,talent:2/3] Dire Beast; [nochanneling,talent:2/2] !Dire Frenzy",
+"/cast [nochanneling,talent:6/1] A Murder of Crows; [nochanneling,talent:6/2] Barrage; ",
 "/cast [nochanneling] Titan's Thunder",
-"/cast [nochanneling] !Kill Command",
-"/cast [nochanneling] Bestial Wrath",
+"/cast [nochanneling] Cobra Shot",
 PostMacro = [[
-/cast Aspect of the Wild
-/use [combat]11
+/cast [combat,nochanneling]Aspect of the Wild
+/cast [combat,nochanneling] Blood Fury
 /use [combat]13
 /use [combat]14
+/script UIErrorsFrame:Clear()
+/console Sound_EnableSFX 1
 ]],
 }
