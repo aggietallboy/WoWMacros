476f5463e183cc840fa996ec3c3dc70338c15df2
aggietallboy <aggietallboy@hotmail.com>
2016-07-21 09:43:28 -0500

    Update WarlockDemonology.lua

--- GS-AggietallboyMacros/Warlock/Demonology-266/WarlockDemonology.lua
+++ GS-AggietallboyMacros/Warlock/Demonology-266/WarlockDemonology.lua
@@ -2,3 +2,17 @@ local Sequences = GSMasterSequences
 ------------------
 ----- Warlock
 ------------------
+Sequences['PUT_NAME_HERE'] = {
+specID = 266,
+author = "WHO_WROTE_IT",
+helpTxt = "INFO ABOUT MACRO/Title - Talent: 3212233",
+StepFunction = GSStaticPriority,
+PreMacro = [[
+PUT PREMACRO HERE
+]],
+"/cast SomeSpells",
+"/cast [nochanneling] Some More spells, but don't knock out one that's running",
+PostMacro = [[
+  cleanup with some more spells that you care about
+]],
+}
