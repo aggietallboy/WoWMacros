1c31059f93b7f8f15c1cab8934200275a846d1a2
aggietallboy <aggietallboy@hotmail.com>
2016-07-21 12:54:48 -0500

    Update HunterMarksmanship.lua
    fixed commas, semi colons, changed up pre and post

--- GS-AggietallboyMacros/Hunter/Marksmanship-254/HunterMarksmanship.lua
+++ GS-AggietallboyMacros/Hunter/Marksmanship-254/HunterMarksmanship.lua
@@ -9,25 +9,27 @@ author = "Aggietallboy",
 helpTxt = "Marksman Single - Talent: 1123121",
 StepFunction = GSStaticPriority,
 PreMacro = [[
+/console Sound_EnableSFX 0
+/script UIErrorsFrame:Hide()
 /targetenemy [noharm][dead]
-/startattack
 /cast [target=focus, exists, nodead],[target=pet, exists, nodead] Misdirection
 ]],
-	"/cast [nochanneling] Windburst;",
-	"/cast [nochanneling] Aimed Shot;",
-	"/cast [nochanneling] Marked Shot;",
-	"/cast [nochanneling, talent:7/2] Piercing Shot"
-	"/cast [nochanneling, talent:6/1] A Murder of Crows;[nochanneling, talent:6/2] Barrage",
+	"/cast [nochanneling] Windburst",
+	"/cast [nochanneling] Aimed Shot",
+	"/cast [nochanneling] Marked Shot",
+	"/cast [nochanneling, talent:7/2] Piercing Shot",
+	"/cast [nochanneling, talent:6/1] A Murder of Crows;[nochanneling, talent:6/2] Barrage;",
 	"/cast [nochanneling, talent:4/1] Explosive Shot",
 	"/cast [nochanneling, talent:4/1] Explosive Shot",
 	"/cast [nochanneling, combat] Aimed Shot",
 	"/cast [nochanneling, talent:2/2] Black Arrow",
 	"/cast [nochanneling, talent:7/1] Sidewinders; [nochanneling] Arcane Shot;",
 PostMacro = [[
-/cast Aspect of the Wild
+/cast [combat,nochanneling]Aspect of the Wild
+/cast [combat,nochanneling] Blood Fury
 /use [combat]13
 /use [combat]14
-/script UIErrorsFrame:Hide();
+/script UIErrorsFrame:Clear()
 /console Sound_EnableSFX 1
 ]],
 }
@@ -41,23 +43,24 @@ StepFunction = GSStaticPriority,
 PreMacro = [[
 /console Sound_EnableSFX 0
 /targetenemy [noharm][dead]
+/cast [target=focus, exists, nodead],[target=pet, exists, nodead] Misdirection
 ]],
-"/cast [nochanneling] Windburst;",
-	"/cast [nochanneling] Aimed Shot;",
-	"/cast [nochanneling] Marked Shot;",
-	"/cast [nochanneling, talent:7/2] Piercing Shot;Multi-Shot"
-	"/cast [nochanneling, talent:6/1] A Murder of Crows;[nochanneling, talent:6/2] Barrage",
+	"/cast [nochanneling] Windburst",
+	"/cast [nochanneling] Aimed Shot",
+	"/cast [nochanneling] Marked Shot",
+	"/cast [nochanneling, talent:7/2] Piercing Shot; Multi-Shot",
+	"/cast [nochanneling, talent:6/1] A Murder of Crows; [nochanneling, talent:6/2] Barrage",
 	"/cast [nochanneling, talent:4/1] Explosive Shot",
 	"/cast [nochanneling, talent:4/1] Explosive Shot",
 	"/cast [nochanneling, combat] Aimed Shot",
 	"/cast [nochanneling, talent:2/2] Black Arrow",
-	"/cast [nochanneling, talent:7/1] Sidewinders; [nochanneling] Multi-Shot;",
+	"/cast [nochanneling, talent:7/1] Sidewinders; [nochanneling] Multi-Shot",
 PostMacro = [[
-/startattack
-/petattack [@target,harm]
+/cast [combat,nochanneling]Aspect of the Wild
+/cast [combat,nochanneling] Blood Fury
 /use [combat]13
 /use [combat]14
-/script UIErrorsFrame:Hide();
+/script UIErrorsFrame:Clear()
 /console Sound_EnableSFX 1
 ]],
 }
