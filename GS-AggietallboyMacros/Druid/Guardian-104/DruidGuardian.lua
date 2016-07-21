e3505d74c17f961f577145a70952ed60ab1d52ba
aggietallboy <aggietallboy@hotmail.com>
2016-07-21 09:28:11 -0500

    Update DruidGuardian.lua

--- GS-AggietallboyMacros/Druid/Guardian-104/DruidGuardian.lua
+++ GS-AggietallboyMacros/Druid/Guardian-104/DruidGuardian.lua
@@ -1,1 +1,18 @@
 local Sequences = GSMasterSequences
+------------------
+----- Druid
+------------------
+Sequences['PUT_NAME_HERE'] = {
+specID = 104,
+author = "WHO_WROTE_IT",
+helpTxt = "INFO ABOUT MACRO/Title - Talent: 3212233",
+StepFunction = GSStaticPriority,
+PreMacro = [[
+PUT PREMACRO HERE
+]],
+"/cast SomeSpells",
+"/cast [nochanneling] Some More spells, but don't knock out one that's running",
+PostMacro = [[
+  cleanup with some more spells that you care about
+]],
+}
